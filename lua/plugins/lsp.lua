2dd5c1881cedbd365aeda9e89c693d6b21a1c144
roy <roy@gmail.com>
2025-02-06 13:26:55 -0500

    diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,29 @@ return {
       vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
     end
+
+    local keymap = vim.keymap -- for conciseness
     
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf, silent = true }
+        opts.desc = "Show line diagnostics"
+        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+        opts.desc = "Go to previous diagnostic"
+        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+        opts.desc = "Go to next diagnostic"
+        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+    end,
+    })
+
     require'lspconfig'.rust_analyzer.setup{
       on_attach = on_attach,
       settings = {
