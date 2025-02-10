7cc367c8046535724fec036b82e3cd89103e6789
roy <roy@gmail.com>
2025-02-10 07:57:33 -0500

    so

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,8 +1,9 @@
 return{
-    -- rust
-    'rust-lang/rust.vim',
-    -- () {} [] '' ""
-    'jiangmiao/auto-pairs',
-    'terryma/vim-multiple-cursors',
+  -- rust
+  'rust-lang/rust.vim',
+  -- () {} [] '' ""
+  'jiangmiao/auto-pairs',
+  'terryma/vim-multiple-cursors',
+  'dhruvasagar/vim-table-mode'
 }
 
